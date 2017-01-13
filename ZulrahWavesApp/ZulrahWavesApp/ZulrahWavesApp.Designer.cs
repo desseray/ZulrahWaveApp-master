cb6f71b62b895f15a1e2ddbd428ebec1a46ab8e2
desseray <Desseray@hotmail.com>
2017-01-13 17:01:00 +1300

    update test
    update test

---
+++ ZulrahWavesApp/ZulrahWavesApp/ZulrahWavesApp.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ZulrahWavesApp
+{
+    partial class ZulrahWavesApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZulrahWavesApp));
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageListRot4 = new System.Windows.Forms.ImageList(this.components);
+            this.imageListRot1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageListRot2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageListRot3 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(402, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(115, 90);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(273, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(115, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(144, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(115, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageListRot4
+            // 
+            this.imageListRot4.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListRot4.ImageStream")));
+            this.imageListRot4.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListRot4.Images.SetKeyName(0, "Phase1.jpg");
+            this.imageListRot4.Images.SetKeyName(1, "Phase2.jpg");
+            this.imageListRot4.Images.SetKeyName(2, "Phase3.jpg");
+            this.imageListRot4.Images.SetKeyName(3, "Phase4.jpg");
+            this.imageListRot4.Images.SetKeyName(4, "Phase5.jpg");
+            this.imageListRot4.Images.SetKeyName(5, "Phase6.jpg");
+            this.imageListRot4.Images.SetKeyName(6, "Phase7.jpg");
+            this.imageListRot4.Images.SetKeyName(7, "Phase8.jpg");
+            this.imageListRot4.Images.SetKeyName(8, "Phase9.jpg");
+            this.imageListRot4.Images.SetKeyName(9, "Phase10.jpg");
+            this.imageListRot4.Images.SetKeyName(10, "Phase11.jpg");
+            this.imageListRot4.Images.SetKeyName(11, "Phase12.jpg");
+            this.imageListRot4.Images.SetKeyName(12, "Phase13.jpg");
+            // 
+            // imageListRot1
+            // 
+            this.imageListRot1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListRot1.ImageStream")));
+            this.imageListRot1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListRot1.Images.SetKeyName(0, "Phase1.jpg");
+            this.imageListRot1.Images.SetKeyName(1, "Phase2.jpg");
+            this.imageListRot1.Images.SetKeyName(2, "Phase3.jpg");
+            this.imageListRot1.Images.SetKeyName(3, "Phase4.jpg");
+            this.imageListRot1.Images.SetKeyName(4, "Phase5.jpg");
+            this.imageListRot1.Images.SetKeyName(5, "Phase6.jpg");
+            this.imageListRot1.Images.SetKeyName(6, "Phase7.jpg");
+            this.imageListRot1.Images.SetKeyName(7, "Phase8.jpg");
+            this.imageListRot1.Images.SetKeyName(8, "Phase9.jpg");
+            this.imageListRot1.Images.SetKeyName(9, "Phase10.jpg");
+            this.imageListRot1.Images.SetKeyName(10, "Phase11.jpg");
+            // 
+            // imageListRot2
+            // 
+            this.imageListRot2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListRot2.ImageStream")));
+            this.imageListRot2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListRot2.Images.SetKeyName(0, "Phase1.jpg");
+            this.imageListRot2.Images.SetKeyName(1, "Phase2.jpg");
+            this.imageListRot2.Images.SetKeyName(2, "Phase3.jpg");
+            this.imageListRot2.Images.SetKeyName(3, "Phase4.jpg");
+            this.imageListRot2.Images.SetKeyName(4, "Phase5.jpg");
+            this.imageListRot2.Images.SetKeyName(5, "Phase6.jpg");
+            this.imageListRot2.Images.SetKeyName(6, "Phase7.jpg");
+            this.imageListRot2.Images.SetKeyName(7, "Phase8.jpg");
+            this.imageListRot2.Images.SetKeyName(8, "Phase9.jpg");
+            this.imageListRot2.Images.SetKeyName(9, "Phase10.jpg");
+            this.imageListRot2.Images.SetKeyName(10, "Phase11.jpg");
+            // 
+            // imageListRot3
+            // 
+            this.imageListRot3.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListRot3.ImageStream")));
+            this.imageListRot3.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListRot3.Images.SetKeyName(0, "Phase1.jpg");
+            this.imageListRot3.Images.SetKeyName(1, "Phase2.jpg");
+            this.imageListRot3.Images.SetKeyName(2, "Phase3.jpg");
+            this.imageListRot3.Images.SetKeyName(3, "Phase4.jpg");
+            this.imageListRot3.Images.SetKeyName(4, "Phase5.jpg");
+            this.imageListRot3.Images.SetKeyName(5, "Phase6.jpg");
+            this.imageListRot3.Images.SetKeyName(6, "Phase7.jpg");
+            this.imageListRot3.Images.SetKeyName(7, "Phase8.jpg");
+            this.imageListRot3.Images.SetKeyName(8, "Phase9.jpg");
+            this.imageListRot3.Images.SetKeyName(9, "Phase10.jpg");
+            this.imageListRot3.Images.SetKeyName(10, "Phase11.jpg");
+            this.imageListRot3.Images.SetKeyName(11, "Phase12.jpg");
+            // 
+            // ZulrahWavesApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            this.ClientSize = new System.Drawing.Size(535, 118);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ZulrahWavesApp";
+            this.Text = "Zulrah Waves App";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(244)))));
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageListRot4;
+        private System.Windows.Forms.ImageList imageListRot1;
+        private System.Windows.Forms.ImageList imageListRot2;
+        private System.Windows.Forms.ImageList imageListRot3;
+    }
+}
+
---
+++ ZulrahWavesApp/ZulrahWavesApp/ZulrahWavesApp.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZulrahWavesApp
+{
+    public partial class ZulrahWavesApp : Form
+    {
+        public int rotOneCount = 2;
+        public int rotTwoCount = 2;
+        public int rotThreeCount = 2;
+        public int rotFourCount = 2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        public ZulrahWavesApp()
+        {
+            InitializeComponent();
+            setupFrames();
+            regHotKeys();
+        }
+
+        public void setupFrames()
+        {
+            this.Top = 100;
+            this.Left = 450;
+            pictureBox1.Image = imageListRot1.Images[1];
+            pictureBox2.Image = imageListRot2.Images[1];
+            pictureBox3.Image = imageListRot3.Images[1];
+            pictureBox4.Image = imageListRot4.Images[1];
+        }
+
+        public void regHotKeys()
+        {
+            //reserves and sets up the hotkeys
+            RegisterHotKey(this.Handle, 1, (int)KeyModifier.None, Keys.D1.GetHashCode());
+            RegisterHotKey(this.Handle, 2, (int)KeyModifier.None, Keys.D2.GetHashCode());
+            RegisterHotKey(this.Handle, 3, (int)KeyModifier.None, Keys.D3.GetHashCode());
+            RegisterHotKey(this.Handle, 4, (int)KeyModifier.None, Keys.D4.GetHashCode());
+            RegisterHotKey(this.Handle, 5, (int)KeyModifier.None, Keys.Escape.GetHashCode());
+            RegisterHotKey(this.Handle, 6, (int)KeyModifier.None, Keys.D0.GetHashCode());
+        }
+
+        //When the message that the hotkey is pressed is recieved handle
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == 0x0312)
+            {
+                Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);                  // The key of the hotkey that was pressed.
+                KeyModifier modifier = (KeyModifier)((int)m.LParam & 0xFFFF);       // The modifier of the hotkey that was pressed.
+                int id = m.WParam.ToInt32();                                        // The id of the hotkey that was pressed.
+
+                switch (key.ToString())
+                {
+                    case "D1":
+                        HandleHotkey1();
+                        break;
+                    case "D2":
+                        HandleHotkey2();
+                        break;
+                    case "D3":
+                        HandleHotkey3();
+                        break;
+                    case "D4":
+                        HandleHotkey4();
+                        break;
+                    case "Escape":
+                        BorderSwitch();
+                        break;
+                    case "D0":
+                        Reset();
+                        break;
+                }
+
+            }
+        }
+
+
+        private void HandleHotkey1()
+        {
+            pictureBox1.Image = imageListRot1.Images[rotOneCount];
+            rotOneCount++;
+
+            switch (rotOneCount)
+            {
+                case 3:
+                    pictureBox3.Image = null;
+                    pictureBox4.Image = null;
+                    break;
+                case 5:
+                    pictureBox2.Image = null;
+                    break;
+                case 11:
+                    rotOneCount = 0;
+                    break;
+            }
+        }
+        private void HandleHotkey2()
+        {
+            pictureBox2.Image = imageListRot2.Images[rotTwoCount];
+            rotTwoCount++;
+
+            switch (rotTwoCount)
+            {
+                case 3:
+                    pictureBox3.Image = null;
+                    pictureBox4.Image = null;
+                    break;
+                case 5:
+                    pictureBox1.Image = null;
+                    break;
+                case 11:
+                    rotTwoCount = 0;
+                    break;
+            }
+        }
+
+        private void HandleHotkey3()
+        {
+            pictureBox3.Image = imageListRot3.Images[rotThreeCount];
+            rotThreeCount++;
+
+            switch (rotThreeCount)
+            {
+                case 12:
+                    rotThreeCount = 0;
+                    break;
+                case 3:
+                    pictureBox1.Image = null;
+                    pictureBox2.Image = null;
+                    pictureBox4.Image = null;
+                    break;
+            }
+
+
+        }
+        private void HandleHotkey4()
+        {
+            pictureBox4.Image = imageListRot4.Images[rotFourCount];
+            rotFourCount++;
+
+            switch (rotFourCount)
+            {
+                case 3:
+                    pictureBox1.Image = null;
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = null;
+                    break;
+                case 13:
+                    rotFourCount = 0;
+                    break;
+            }
+
+        }
+        private void BorderSwitch()
+        {
+            if (this.FormBorderStyle == FormBorderStyle.None)
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+            }
+        }
+
+        private void Reset()
+        {
+            rotOneCount = 2;
+            rotTwoCount = 2;
+            rotThreeCount = 2;
+            rotFourCount = 2;
+            pictureBox1.Image = imageListRot1.Images[1];
+            pictureBox2.Image = imageListRot2.Images[1];
+            pictureBox3.Image = imageListRot3.Images[1];
+            pictureBox4.Image = imageListRot4.Images[1];
+        }
+
+
+
+        private void ZulrahWaves_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 1);
+            UnregisterHotKey(this.Handle, 2);
+            UnregisterHotKey(this.Handle, 3);
+            UnregisterHotKey(this.Handle, 4);
+            UnregisterHotKey(this.Handle, 5);
+            UnregisterHotKey(this.Handle, 6);
+
+        }
+
+
+    }
+}
